906181ec6a0e1cecaae4b8d5930135f8b1255a8f
aezzahir <aero.glowsky@gmail.com>
2023-12-17 15:07:35 +0100

    3. MySQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges on the hbnb_dev_db database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
