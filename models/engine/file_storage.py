1622d9eec862f81567e38fc1eff347fcdfdc730b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-17 20:07:27 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,5 +55,5 @@ class FileStorage:
             return
 
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        if key in self.__objects.keys:
+        if key in self.__objects:
             del self.__objects[key]
