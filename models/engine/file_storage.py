8dfac7290b8472a162cc9acca8e7d110828c330a
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-17 20:10:55 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
