dbec931d9dc9a79d63ed338bb05856544a55be99
aezzahir <aero.glowsky@gmail.com>
2023-12-18 15:06:31 +0100

    6. DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -5,8 +5,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
-    """ State class """
-
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
