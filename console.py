e4c393746d0e1301f5f5c6c18eaabde522f6e751
aezzahir <aero.glowsky@gmail.com>
2023-12-18 15:14:19 +0100

    6. DBStorage - States and Cities

--- console.py
+++ console.py
@@ -176,7 +176,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage._FileStorage__objects[key])
+            print(storage.__objects[key])
         except KeyError:
             print("** no instance found **")
 
@@ -227,11 +227,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.__objects.items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.__objects.items():
                 print_list.append(str(v))
 
         print(print_list)
