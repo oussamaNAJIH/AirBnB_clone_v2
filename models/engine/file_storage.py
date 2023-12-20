a4c48c8ba2c8f85d3e108d88cc8d8c282673d91c
oussamaNAJIH <oussamanajih6@gmail.com>
2023-12-20 21:35:38 +0100

    Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,7 +66,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """Delete obj from __objects if it's inside"""
-        if obj is not None:
+        if obj:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key in self.__objects:
                 del self.__objects[key]
