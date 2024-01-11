c077776778bc93ffb5e3bca348f2c8eaf60a220b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-11 12:00:29 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,6 +5,7 @@ using the function do_deploy
 """
 from fabric.api import *
 import os
+
 env.hosts = ['100.25.102.204', '54.236.41.224']
 
 
@@ -18,14 +19,24 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        archive_without_ext = archive_path.split("/")[-1].split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}".format(archive_without_ext))
+        archive_without_ext = os.path.basename(archive_path).split(".")[0]
         destination = "/data/web_static/releases/{}".format(archive_without_ext)
         source = "/tmp/{}".format(archive_without_ext)
+        # Create the release directory
+        run("mkdir -p {}".format(destination))
+        # Extract the contents to the release directory
         run("tar -xzvf {} -C {}".format(source, destination))
+        # Remove the temporary archive file
         run("rm {}".format(source))
+        # Remove the current symbolic link
         run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(source))
+        # Move contents from web_static subdirectory to release directory
+        run('mv {}/web_static/* {}'.format(destination, destination))
+        # Remove the web_static directory within the release directory
+        run('rm -rf {}/web_static'.format(destination))
+        # Create a new symbolic link
+        run('ln -s {}/ /data/web_static/current'.format(destination))
         return True
-    except:
+    except Exception as e:
+        print("Error: {}".format(str(e)))
         return False
