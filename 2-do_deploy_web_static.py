a505655783d4b49eb6e352c15883450aeb03ca3b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-11 12:16:28 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,17 +22,21 @@ def do_deploy(archive_path):
         # Create the release directory
         run('mkdir -p {}{}/'.format(release_path, no_extension))
         # Extract the contents to the release directory
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, release_path, no_extension))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(
+            file_name, release_path, no_extension))
         # Remove the temporary archive file
         run('rm /tmp/{}'.format(file_name))
         # Move contents from web_static subdirectory to release directory
-        run('mv {}/web_static/* {}/'.format(release_path + no_extension, release_path + no_extension))
+        run('mv {}/web_static/* {}/'.format(
+            release_path + no_extension, release_path + no_extension))
         # Remove the web_static directory within the release directory
         run('rm -rf {}/web_static'.format(release_path + no_extension))
         # Remove the current symbolic link
         run('rm -rf /data/web_static/current')
         # Create a new symbolic link
-        run('ln -s {}/ /data/web_static/current'.format(release_path + no_extension))
+        run('ln -s {}/ /data/web_static/current'.format(
+            release_path + no_extension))
         return True
-    except:
+    except Exception as e:
+        print("Error: {}".format(str(e)))
         return False
