64a28a8ecf5985e427de0c3f428fc5bd0b524f3d
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-11 12:08:19 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,33 +10,29 @@ env.hosts = ['100.25.102.204', '54.236.41.224']
 
 
 def do_deploy(archive_path):
-    """
-    Returns True if all operations have been done correctly
-    otherwise returns False
-    """
+    """distributes an archive to the web servers"""
     if not os.path.exists(archive_path):
         return False
-
     try:
-        put(archive_path, "/tmp/")
-        archive_without_ext = os.path.basename(archive_path).split(".")[0]
-        destination = "/data/web_static/releases/{}".format(archive_without_ext)
-        source = "/tmp/{}".format(archive_without_ext)
+        file_name = archive_path.split("/")[-1]
+        no_extension = file_name.split(".")[0]
+        release_path = "/data/web_static/releases/"
+        # Upload the archive to /tmp/
+        put(archive_path, '/tmp/')
         # Create the release directory
-        run("mkdir -p {}".format(destination))
+        run('mkdir -p {}{}/'.format(release_path, no_extension))
         # Extract the contents to the release directory
-        run("tar -xzvf {} -C {}".format(source, destination))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, release_path, no_extension))
         # Remove the temporary archive file
-        run("rm {}".format(source))
+        run('rm /tmp/{}'.format(file_name))
+        # Move contents from web_static subdirectory to release directory
+        run('mv {}/web_static/* {}/'.format(release_path + no_extension, release_path + no_extension))
+        # Remove the web_static directory within the release directory
+        run('rm -rf {}/web_static'.format(release_path + no_extension))
         # Remove the current symbolic link
         run('rm -rf /data/web_static/current')
-        # Move contents from web_static subdirectory to release directory
-        run('mv {}/web_static/* {}'.format(destination, destination))
-        # Remove the web_static directory within the release directory
-        run('rm -rf {}/web_static'.format(destination))
         # Create a new symbolic link
-        run('ln -s {}/ /data/web_static/current'.format(destination))
+        run('ln -s {}/ /data/web_static/current'.format(release_path + no_extension))
         return True
-    except Exception as e:
-        print("Error: {}".format(str(e)))
+    except:
         return False
