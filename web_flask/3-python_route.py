f65e0dcabe7bbdfe239bd396519973dff4db09d1
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-25 18:06:52 +0100

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    displays hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    displays HBNB
+    """
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    display “C ” followed by the value of the text variable
+    """
+    return f"C {escape(text)}".replace("_", " ")
+
+
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """
+    display “Python” followed by the value of the text variable
+    """
+    return f"Python {escape(text)}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
