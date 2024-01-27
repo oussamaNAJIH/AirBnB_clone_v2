f592379393825935c395287fa6b5a68a3a290ead
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-27 12:12:17 +0100

    Improve engines

--- models/state.py
+++ models/state.py
@@ -3,8 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
+import os
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if (os.HBNB_TYPE_STORAGE == "db"):
+        cities = relationship("City", backref="state")
+    else:
+        @property
+        def cities(self):
+            "return the list of City objects"
+            dict_all_cities = self.all(City)
+            list_cities = []
+            for city in dict_all_cities.values():
+                if city.id == self.state_id:
+                    list_cities.append(city)
+            return list_cities
+            
