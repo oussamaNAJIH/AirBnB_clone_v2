ba1fcf2bc3aee60d7ba784ff6c8e98de72c151e8
oussamaNAJIH <oussamanajih6@gmail.com>
2024-01-27 12:16:53 +0100

    Improve engines

--- models/state.py
+++ models/state.py
@@ -9,27 +9,18 @@ import os
 from os import environ
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
     if environ.get('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City",
-                              backref="state",
+        cities = relationship("City", backref="state",
                               cascade="all, delete, delete-orphan")
     else:
-
         @property
         def cities(self):
-            """ Returns the list of City instances with
-            state_id == current State.id """
-            all_cities = models.storage.all(City)
-            state_cities = []
-            for city_ins in all_cities.values():
-                if city_ins.state_id == self.id:
-                    state_cities.append(city_ins)
-
-            return state_cities
+            "return the list of City objects"
+            dict_all_cities = models.storage.all(City)
+            list_cities = []
+            for city in dict_all_cities.values():
+                if city.id == self.state_id:
+                    list_cities.append(city)
+            return list_cities
